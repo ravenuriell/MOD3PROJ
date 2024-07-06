96ddf00072baa378b98ca168ccb8eeac12218621
Raven Uriel <91958584+ravenuriell@users.noreply.github.com>
2024-07-07 04:53:48 +0800

    Add files via upload

---
+++ mod3.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LustCoin is ERC20, Ownable {
+    constructor(address initialOwner, string memory name, string memory symbol) ERC20(name, symbol) Ownable(initialOwner){
+        // Validate initial owner address
+        require(initialOwner != address(0), "Invalid initial owner");
+
+        uint256 initialSupply = 1000; 
+        _mint(initialOwner, initialSupply);
+
+        transferOwnership(initialOwner);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
+
+
