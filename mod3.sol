f6532ef8e18144e6dcb0ad26d22e6a67b6e1bb61
Raven Uriel <91958584+ravenuriell@users.noreply.github.com>
2024-07-19 02:13:27 +0800

    Update mod3.sol
    Explicitly added the transfer function

--- mod3.sol
+++ mod3.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LustCoin is ERC20, Ownable {
-    constructor(address initialOwner, string memory name, string memory symbol) ERC20(name, symbol) Ownable(initialOwner){
+    constructor(address initialOwner, string memory name, string memory symbol) ERC20(name, symbol) Ownable(initialOwner) {
         // Validate initial owner address
         require(initialOwner != address(0), "Invalid initial owner");
 
@@ -22,6 +22,10 @@ contract LustCoin is ERC20, Ownable {
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
     }
+    //explicitly added transfer function in the code
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
 }
-
-
